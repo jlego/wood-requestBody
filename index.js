19ae900654c0057deb33135b01c79eb4941ecb41
jlego <273734893@qq.com>
2018-11-24 19:08:29 +0800

    no message

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ module.exports = (app = {}, config = {}) => {
   app.requestbody = function(req, res, next){
     if (req.method == 'PUT') {
       if (req.body) {
-        if (!req.body.cmd) {
-          res.print(app.error_code.error_body_cmd);
-          return;
-        } else if (!req.body.data) {
-          res.print(app.error_code.error_body_data);
+        if (!req.body.data) {
+          res.print(app.config.errorCode.error_body_data);
           return;
         }
       } else {
-        res.print(app.error_code.error_body);
+        res.print(app.config.errorCode.error_body);
         return;
       }
     }
